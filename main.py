1eb0f0d1038a5c6d7c6137fe162c5f8c6558fa36
Keven Nogueira <kevenbjj7@gmail.com>
2025-05-09 11:38:43 -0300

    Adiciona flask-sqlalchemy e atualiza requirements.txt

--- main.py
+++ main.py
@@ -1,31 +1,67 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
-# Primeiro cria a instância do SQLAlchemy
-db = SQLAlchemy()
-
-# Depois cria a aplicação Flask
 app = Flask(__name__)
-
-# Configuração do banco de dados
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reservas.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# Associa a instância do SQLAlchemy com a app
-db.init_app(app)
+db = SQLAlchemy(app)
 
 class Reserva(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100))
     telefone = db.Column(db.String(20))
-    # ... (outras colunas)
+    data = db.Column(db.String(20))
+    pessoas = db.Column(db.Integer)
+    evento = db.Column(db.String(50))
+    obs = db.Column(db.String(200))
 
-# Cria as rotas normalmente
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/reservas', methods=['GET', 'POST'])
+def reservas():
+    if request.method == 'POST':
+        data = request.json
+        nova = Reserva(
+            nome=data['nome'],
+            telefone=data['telefone'],
+            data=data['data'],
+            pessoas=data['pessoas'],
+            evento=data['evento'],
+            obs=data['obs']
+        )
+        db.session.add(nova)
+        db.session.commit()
+        return jsonify({'mensagem': 'Reserva salva com sucesso!'})
+    else:
+        todas = Reserva.query.all()
+        return jsonify([{
+            'id': r.id,
+            'nome': r.nome,
+            'telefone': r.telefone,
+            'data': r.data,
+            'pessoas': r.pessoas,
+            'evento': r.evento,
+            'obs': r.obs
+        } for r in todas])
+
+@app.route('/deletar/<int:id>', methods=['POST'])
+def deletar(id):
+    data = request.json
+    if data.get('senha') == '147952':
+        reserva = Reserva.query.get(id)
+        if reserva:
+            db.session.delete(reserva)
+            db.session.commit()
+            return jsonify({'mensagem': 'Reserva deletada'})
+        else:
+            return jsonify({'erro': 'Reserva não encontrada'}), 404
+    return jsonify({'erro': 'Senha incorreta'}), 401
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+    db.create_all()
     app.run(debug=True)
